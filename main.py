08df92aee40bfba9eaa1ea3d86a3a807af02ee47
ajamol2197 <ajamol2197@gmail.com>
2025-10-31 17:59:54 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1
+for a in range(11):
+    print(a)
+
+# 2
+for a in range(10,1, -1):
+    print(a)
+
+# 3
+n =int(input("son kirit: "))
+
+for i in range(2, n, 2):
+    print(i)
+
+
+# 4
+n =int(input("son kirit: "))
+
+for i in range(1, n, 2):
+    print(i)
